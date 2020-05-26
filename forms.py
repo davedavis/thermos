ed26bd0f791857a9833f6219f969a85d85fc7dbe
Dave Davis <dave@redflymarketing.com>
2020-05-26 23:46:47 +0100

    Add validation to form, add form macro taking labels and improve error notification.

--- forms.py
+++ forms.py
@@ -5,6 +5,20 @@ from wtforms.validators import DataRequired, url
 from wtforms.fields.core import StringField
 
 
+# Class that uses the FlaskForm WTF class and and creates a simple form with two fields.
 class BookmarkForm(FlaskForm):
-    url = URLField('url', validators=[DataRequired(), url()])
-    description = StringField('description')
+    url = URLField('Please enter a URL', validators=[DataRequired(), url()])
+    description = StringField('Add an optional description')
+
+    # Override the validate method to add some custom validation not included with WTForms.
+    def validate(self):
+        # TODO: Improve UX in validating when user doesn't enter a protocol. Fix reappending of incorrect URL form data.
+        # if not self.url.data.startswith("http://") or self.url.data.startswith("https://"):
+        #     self.url.data = "https://" + self.url.data
+        # if not FlaskForm.validate(self):
+        #     return False
+
+        if not self.description.data:
+            self.description.data = self.url.data
+
+        return True
--- thermos.py
+++ thermos.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 # Store it in the apps config dict.
 app.config['SECRET_KEY'] = '\x83\xbf\x94\x19\x91\xd9:\x9a\x82\x12K\xbc\xa2\xc1f\xde\xc9\xbb\xa7\x82\xdd\t\xbb\xc7'
 
+# Temporary structure to hold bookmarks, pending DB.
 bookmarks = []
 
 
+# Temporary method to store bookmarks in the temporary dict.
 def store_bookmarks(url, description):
     bookmarks.append(dict(
         url=url,
@@ -19,6 +21,7 @@ def store_bookmarks(url, description):
     ))
 
 
+# Method that gets and sorts bookmarks to be displayed.
 def new_bookmarks(num):
     return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
 
@@ -29,18 +32,23 @@ def index():
     return render_template('index.html', new_bookmarks=new_bookmarks(5))
 
 
+# If the method is POST, process the form. If it's GET, create and show the form.
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    # Create a new BookmarkForm instance.
     form = BookmarkForm()
+    # Validate the form, store the bookmarks and redirect to the index.
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
         store_bookmarks(url, description)
-        flash("Stored'{}'".format(description))
+        flash("Stored '{}'".format(description))
         return redirect(url_for('index'))
+    # Render the add template giving it the empty form object.
     return render_template('add.html', form=form)
 
 
+# Error handling.
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
