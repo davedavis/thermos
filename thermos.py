b023ac2c910c0d87afe95b8027b432387f9905df
Dave Davis <dave@redflymarketing.com>
2020-06-01 00:03:39 +0100

    Add Flask-Script functionality.

--- thermos.py
+++ thermos.py
@@ -1,22 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import datetime
+
+from sqlalchemy import desc
+
 from forms import BookmarkForm
+# import models
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 app = Flask(__name__)
-
 
 # Store it in the apps config dict. This is needed for the flash function and and to access the session object.
 app.config['SECRET_KEY'] = '\x83\xbf\x94\x19\x91\xd9:\x9a\x82\x12K\xbc\xa2\xc1f\xde\xc9\xbb\xa7\x82\xdd\t\xbb\xc7'
 
 # SQLAlchemy setup.
 # Suppress the annoying deprecation warning in the console.
-app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # ToDo: Switch between SQLite and MySQL in dev/production. Although I could do with experience in MySQL in dev.
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
 # pip install mysqlclient to get the SQLAlchemy MySQL support.
@@ -25,29 +26,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://thermos:thermos@localhost/therm
 db = SQLAlchemy(app)
 
 
-# Temporary structure to hold bookmarks, pending DB.
-bookmarks = []
-
-
-# Temporary method to store bookmarks in the temporary dict.
-def store_bookmarks(url, description):
-    bookmarks.append(dict(
-        url=url,
-        description=description,
-        user="Dave",
-        date=datetime.utcnow()
-    ))
-
-
 # Method that gets and sorts bookmarks to be displayed.
 def new_bookmarks(num):
-    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
+    return []
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', new_bookmarks=new_bookmarks(5))
+    # ToDo: Don't Forget to change the Bookmark.newest to models.Bookmark.newest when circular import is resolved.
+    return render_template('index.html', new_bookmarks=Bookmark.newest(5))
 
 
 # If the method is POST, process the form. If it's GET, create and show the form.
@@ -59,7 +47,9 @@ def add():
     if form.validate_on_submit():
         url = form.url.data
         description = form.description.data
-        store_bookmarks(url, description)
+        bm = Bookmark(url=url, description=description)
+        db.session.add(bm)
+        db.session.commit()
         flash("Stored '{}'".format(description))
         return redirect(url_for('index'))
     # Render the add template giving it the empty form object.
@@ -81,6 +71,36 @@ def server_error(e):
 def method_not_allowed(e):
     return render_template('405.html'), 405
 
+
+######################################################################################################
+################     Remove This and Fix Circular Import Issue      ##################################
+######################################################################################################
+
+class Bookmark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    description = db.Column(db.String(300))
+
+    @staticmethod
+    def newest(num):
+        return Bookmark.query.order_by(desc(Bookmark.date)).limit(num)
+
+    def __repr__(self):
+        return "Bookmark '{}': '{}'>".format(self.description, self.url)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+######################################################################################################
+################     End Models Class      ##################################
+######################################################################################################
 
 if __name__ == '__main__':
     app.run()
